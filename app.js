76f33e388a2b8727571c3953b1fcb4d63f3c7c18
张景浩 <zhangjinghao@hrsoft.net>
2017-12-22 18:41:44 +0800

    增加后端判断账号密码错误功能

--- app.js
+++ app.js
@@ -14,31 +14,41 @@ app.use(bodyParser())
 
 app.use(cors({
   origin: (ctx) => {
-    if(ctx.url === '/api/login'){
+    if (ctx.url === '/api/login') {
       return '*'
     }
-  },exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  }, exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
   maxAge: 5,
   credentials: true,
   allowMethods: ['GET', 'POST', 'DELETE'],
-  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept']
 }))
 
 router.prefix('/api')
-  .get('/test',(ctx,next)=>{
-    ctx.response.body=`<h1>This is a test~</h2>`
+  .get('/test', (ctx, next) => {
+    ctx.response.body = `<h1>This is a test~</h2>`
   })
   .post('/login', (ctx, next) => {
     if (ctx.request.body.name === 'root' && ctx.request.body.password === '123456') {
-      ctx.response.type='application/json'
+      ctx.response.type = 'application/json'
       ctx.response.body = {
         code: 0
       }
+    } else if (ctx.request.body.name !== 'root') {
+        ctx.response.type = 'application/json'
+        ctx.response.body = {
+          code: 1001
+        }
+      }else if (ctx.request.body.name === 'root' && ctx.request.body.password !== '123456') {
+        ctx.response.type = 'application/json'
+        ctx.response.body = {
+          code: 1002
+        }
       }else {
-      ctx.response.type='application/json'
-      ctx.response.body = {
-        code: 1002
-      }
+        ctx.response.type = 'application/json'
+        ctx.response.body = {
+          code: 1003
+        }
     }
   })
 app.use(router.routes())
